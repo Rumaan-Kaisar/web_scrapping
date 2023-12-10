d5169fddab6d5ba649cea3910f061de81e5bf42a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-10 20:57:50 +0600

    Update friend_req.js

--- FB_scripts/friend_req.js
+++ FB_scripts/friend_req.js
@@ -145,7 +145,7 @@ const delay = (delayInms) => {
 const sleep_baby = async () => {
     console.log('a');
     console.log('waiting...');
-    let delayres = await delay(Math.random()*30000);
+    let delayres = await delay(Math.random()*50000);
     console.log('b');
 };
 
@@ -164,5 +164,41 @@ for (let i = 0; i < (inputs.length -1); i++) {
             inputs[i].getElementsByClassName('x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft')[1].click();   // works
         } 
     }
-    sleep_baby();
+    sleep_baby();   // Issue: delayed after all click happened :(
+}
+
+
+
+
+// ####################################       fixing Waiting issues       ####################################
+
+// -=-=-=-=-=-=-=-=-    Send  Friend Requests (with wait)    -=-=-=-=-=-=-=-=-
+// wait mechanism
+const delay = (delayInms) => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
+};
+
+const sleep_baby = async (i) => {
+    console.log('a');
+    console.log('waiting...');
+    let delayres = await delay(Math.random()*50000);
+    console.log('b');
+    inputs[i].getElementsByClassName('x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft')[1].click();   // works
+};
+
+
+// sending request
+var inputs = document.getElementsByClassName('x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1r8uery x1iyjqo2 xs83m0k x1qughib x11i5rnm x1mh8g0r x2lwn1j xeuugli x4uap5 xkhd6sd xz9dl7a xsag5q8 x1n2onr6 x1ja2u2z x6s0dn4 x1q0g3np x1a02dak x4cne27 xifccgj');
+console.log(inputs);
+
+// ERR check : for no mutual friends
+for (let i = 0; i < (inputs.length -1); i++) {
+    // first check any mutual friends
+    if(inputs[i].getElementsByClassName('x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1nxh6w3 x1sibtaa xo1l8bm xi81zsa')[1]){
+        console.log(parseInt(inputs[i].getElementsByClassName('x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1nxh6w3 x1sibtaa xo1l8bm xi81zsa')[1].innerText.split(' ')[0]));
+        let mutual_friends = parseInt(inputs[i].getElementsByClassName('x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1nxh6w3 x1sibtaa xo1l8bm xi81zsa')[1].innerText.split(' ')[0]);
+        if (mutual_friends > 100) {
+            sleep_baby(i);  // also works !!
+        } 
+    }
 }
